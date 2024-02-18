e4617377141cd975cc91b02097f1dfec7ef9247f
nicop <proskauer.n@northeastern.edu>
2024-02-17 20:13:08 -0500

    playercontroller with no gravity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 3f;
+    public float mouseSensitivity = 175f;
+
+    private Camera playerCamera;
+    CharacterController characterController;
+
+    void Start()
+    {
+        // Assuming the camera is a child of the player
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        // Player movement based on camera direction
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = playerCamera.transform.forward * vertical + playerCamera.transform.right * horizontal;
+
+        // Optionally, you can add jumping logic or other actions here
+        characterController.Move(movementDirection.normalized * movementSpeed * Time.deltaTime);
+    }
+}
